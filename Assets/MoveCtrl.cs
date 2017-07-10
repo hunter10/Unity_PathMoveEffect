1e5cca5d3e784990e70bd327bd0ccc731b933de1
DESKTOP-NBE0U7N\hunter <hunter10@naver.com>
2017-07-10 16:55:16 +0900

    1차 완료

---
+++ Assets/MoveCtrl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class MoveCtrl : MonoBehaviour {
+
+    private Transform tr;
+    Transform tempT;
+
+    private Vector3[] paths = new Vector3[] { new Vector3(-8, 4, 0), new Vector3(-4, -4, 0), new Vector3(0, 4, 0), new Vector3(4, -4, 0), new Vector3(8, 4, 0)};
+    public float moveSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
+
+        tr = GetComponent<Transform>();
+
+        //tr.position = new Vector3(-8.0f, 4.0f, 0f);
+    }
+
+    // Update is called once per frame
+    bool moveStart = false;
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            moveStart = true;
+        }
+
+        if (moveStart == true)
+        {
+            
+            //tr.Translate(paths[1] * Time.deltaTime * moveSpeed, Space.World);
+            tr.position = paths[1] * Time.deltaTime * moveSpeed;
+
+            //tr.DOMove(paths[1], 1).SetRelative().SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+
+    public Vector3 LocalToGlobalTransVector(Vector3 v)
+    {
+        //0기준으로 벡터 크기가 글로벌 크기로 얼마인지를 반환 ..
+        tempT.localPosition = v;
+        return tempT.position;
+    }
+
+
+    public Vector3 GlobalToLocalTransVector(Vector3 v)
+    {
+        //0기준으로 벡터 크기가 글로벌 크기로 얼마인지를 반환 ..
+        tempT.position = v;
+        return tempT.localPosition;
+    }
+    
+}
---
+++ Assets/PathFollower.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MOVERTYPE
+{
+    MOVER,
+    FOLLOWER,
+}
+
+public class PathFollower : MonoBehaviour {
+
+    public Transform[] path;
+    public float speed = 5.0f;
+    public float reachDist = 1.0f;
+    public int currentPoint = 0;
+
+    public MOVERTYPE currType;
+
+	void Start () {
+
+        StartCoroutine(MoveStart());
+	}
+	
+	void Update () {
+
+        if (moveStart == true)
+        {
+            // Ease 방식 - 목적지에 가까울수록 느려짐
+            //Vector3 dir = path[currentPoint].position - transform.position;
+            //transform.position += dir * Time.deltaTime * speed;
+            //if (dir.magnitude <= reachDist)
+            //{
+            //    currentPoint++;
+            //}
+
+            // linear 방식 - 방향만 정해서 움직이기
+            float dist = Vector3.Distance(path[currentPoint].position, transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, path[currentPoint].position, Time.deltaTime * speed);
+            if (dist <= reachDist)
+            {
+                currentPoint++;
+            }
+
+
+            // 다 도착했으면 트레일 렌더러 HIDE
+            if (currentPoint >= path.Length)
+            {
+                currentPoint = path.Length - 1;
+            }
+        }
+	}
+
+    bool moveStart = false;
+    IEnumerator MoveStart()
+    {
+        if (currType == MOVERTYPE.FOLLOWER)
+            yield return new WaitForSeconds(0.2f);
+        else
+            yield return new WaitForEndOfFrame();
+
+        moveStart = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(path.Length > 0)
+        for(int i = 0; i < path.Length; i++)
+        {
+            if(path[i] != null)
+            {
+                Gizmos.DrawSphere(path[i].position, reachDist);
+            }
+        }
+    }
+}
